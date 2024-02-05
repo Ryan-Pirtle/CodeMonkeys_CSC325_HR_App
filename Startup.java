d4a899e7cae4eb45ead9e7af6c1cc17051f05412
Ryan Pirtle <rpirtle320@comcast.net>
2024-02-05 13:41:34 -0600

    StartUp class

--- Startup.java
+++ Startup.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 public class Startup {
     private ArrayList<String> locationCells;
+
+    //set location to passed in arrayList
     private void setLocationCells(ArrayList<String> locs){
         locationCells = locs;
     }
+    //if hit remove index of hit and if miss return that
+    //if locationCells becomes empty return kill
     private String checkYourself(String userInput){
         String result = "miss";
         int index = locationCells.indexOf(userInput);
